c619f35dd3bbd2bb0ddfee6a4a21d19e56b9d3a7
Vitaly Kravchenko <vitalyx@gmail.com>
2018-11-30 10:26:12 +0000

    Clamping scale test.

--- src/scale/LinearScale.test.ts
+++ src/scale/LinearScale.test.ts
@@ -25,6 +25,21 @@ test('convert linear', () => {
     expect(scale.convert(-100)).toBe(0);
     expect(scale.convert(0)).toBe(50);
     expect(scale.convert(100)).toBe(100);
+
+    // non-clamping
+    expect(scale.convert(-300)).toBe(-100);
+    expect(scale.convert(300)).toBe(200);
+});
+
+test('convert linear clamp', () => {
+    const scale = scaleLinear();
+
+    scale.domain = [-100, 100];
+    scale.range = [0, 100];
+    scale.clamp = true;
+
+    expect(scale.convert(-300)).toBe(0);
+    expect(scale.convert(300)).toBe(100);
 });
 
 test('invert linear', () => {
@@ -36,6 +51,21 @@ test('invert linear', () => {
     expect(scale.invert(50)).toBe(0);
     expect(scale.invert(0)).toBe(-100);
     expect(scale.invert(75)).toBe(50);
+
+    // non-clamping
+    expect(scale.invert(-50)).toBe(-200);
+    expect(scale.invert(150)).toBe(200);
+});
+
+test('invert linear clamp', () => {
+    const scale = scaleLinear();
+
+    scale.domain = [-100, 100];
+    scale.range = [0, 100];
+    scale.clamp = true;
+
+    expect(scale.invert(-50)).toBe(-100);
+    expect(scale.invert(150)).toBe(100);
 });
 
 test('convert polylinear', () => {
