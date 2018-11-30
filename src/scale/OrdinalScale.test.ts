24b46f166442bc0b325bd2235f9fb98c4d7f0113
Vitaly Kravchenko <vitalyx@gmail.com>
2018-11-30 16:12:08 +0000

    Ordinal scale fixes and tests.

--- src/scale/OrdinalScale.test.ts
+++ src/scale/OrdinalScale.test.ts
@@ -1,6 +1,6 @@
 import {OrdinalScale} from "./OrdinalScale";
 
-test('populating domain', () => {
+test('implicit unknown', () => {
     const scale = new OrdinalScale();
 
     expect(scale.convert('B')).toBe(undefined);
@@ -8,4 +8,40 @@ test('populating domain', () => {
     expect(scale.convert('A')).toBe(undefined);
 
     expect(scale.domain).toEqual(['B', 'C', 'A']);
+    expect(scale.range).toEqual([]);
+});
+
+test('explicit unknown', () => {
+    const scale = new OrdinalScale();
+
+    scale.unknown = 'Joe';
+
+    expect(scale.convert('B')).toBe('Joe');
+    expect(scale.convert('A')).toBe('Joe');
+
+    expect(scale.domain).toEqual([]);
+    expect(scale.range).toEqual([]);
+});
+
+test('domain/range mapping', () => {
+    const scale = new OrdinalScale();
+
+    scale.domain = ['A', 'B', 'C', 'D', 'E'];
+    scale.range = [0, 1, 2, 3, 4];
+
+    expect(scale.domain).toEqual(['A', 'B', 'C', 'D', 'E']);
+    expect(scale.range).toEqual([0, 1, 2, 3, 4]);
+
+    expect(scale.convert('A')).toBe(0);
+    expect(scale.convert('B')).toBe(1);
+    expect(scale.convert('C')).toBe(2);
+    expect(scale.convert('D')).toBe(3);
+    expect(scale.convert('E')).toBe(4);
+
+    expect(scale.convert('X')).toBe(0);
+    expect(scale.convert('Y')).toBe(1);
+    expect(scale.convert('Z')).toBe(2);
+
+    expect(scale.domain).toEqual(['A', 'B', 'C', 'D', 'E', 'X', 'Y', 'Z']);
+    expect(scale.range).toEqual([0, 1, 2, 3, 4]);
 });
--- src/scale/OrdinalScale.ts
+++ src/scale/OrdinalScale.ts
@@ -5,30 +5,21 @@ export class OrdinalScale<D, R> implements Scale<D, R> {
     _range: R[] = [];
 
     private index = new Map<D, number>();
-    // If `unknown` is set, the `convert` returns the `unknown`, if the value passed to `convert` is
-    // not in the domain. If `unknown` is not set (default), and the value passed to the `convert` is not in
-    // the domain, it is added to the domain.
+
+    /**
+     * If {@link unknown} is set, the {@link convert} returns the `unknown`, if the value passed to `convert` is
+     * not in the domain. If `unknown` is not set (default), and the value passed to the `convert` is not in
+     * the domain, it is added to the domain.
+     */
     unknown?: R;
 
     convert(value: D): R {
-        if (!this.index.has(value)) {
-            this.index.set(value, this._domain.push(value) - 1);
-        }
-
-        const i = this.index.get(value);
+        let i = this.index.get(value);
         if (i === undefined) {
-            if (this.unknown !== undefined) {
-                return this.unknown;
-            }
+            if (this.unknown !== undefined) return this.unknown;
+            this.index.set(value, i = this._domain.push(value) - 1);
         }
-        if (!this._range.length) {
-            if (this.unknown === undefined) {
-                throw new Error('The unknown value is not set');
-            }
-            return this.unknown;
-        } else {
-            return this._range[i % this._range.length];
-        }
+        return this._range[i % this._range.length];
     }
 
     set domain(values: D[]) {
@@ -56,8 +47,4 @@ export class OrdinalScale<D, R> implements Scale<D, R> {
     get range(): R[] {
         return this._range;
     }
-}
-
-export default function scaleOrdinal() {
-
 }
